7176f89711b7e982bfc75fc73a9e55e45a492ad8
Justin <justineder@gmail.com>
2017-04-03 23:30:21 -0500

    STILL messing with heroku

--- imports/ui/pages/Index.js
+++ imports/ui/pages/Index.js
@@ -2,14 +2,24 @@ import React from 'react';
 import { Jumbotron, Button } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import Login from './Login';
+import { Link } from 'react-router';
+
 
 class Index extends React.Component {
+  componentDidMount() {
+    this.props.changeBg(0);
+  }
 
   render() {
     return (
-      <div className="index">    
-        <Button bsStyle="warning" className="enter-button">ENTER</Button>
-      </div>
+
+         
+            
+      <Link to={'/login'}>
+        <div className="index">    
+          <Button bsStyle="warning" className="enter-button">ENTER</Button>
+        </div>
+      </Link>
     )
   }
 }
